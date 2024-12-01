2052555ef1b4a47c8c1662318ddc1ccc5c904547
changhoyu <tyu9817@naver.com>
2024-12-01 16:29:24 +0900

    ♻️ Refactor: 함수 표현식 -> 선언식 변경

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import HomePage from '../../pages/HomePage';
-import logo from "@svgs/logo_horizon.svg";
-import instagram from "@svgs/instagram.svg";
-import github from "@svgs/github.svg";
+import logo from '@svgs/logo_horizon.svg';
+import instagram from '@svgs/instagram.svg';
+import github from '@svgs/github.svg';
 
-
-const Footer = () => {
+export default function Footer() {
   return (
     <div className={styles.wrapper}>
-        <img src={logo} alt="logo" onClick={HomePage} />
-          <li>
-            <a href="https://www.instagram.com/likelion_skuniv" target="_blank" rel="noopener noreferrer">
-              <img src={instagram} alt="instagram"/> </a>
-            <a href="https://github.com/LikeLion-SKU" target="_blank" rel="noopener noreferrer">
-              <img src={github} alt="github"/> </a>
-          </li>
-        <p>© 2024 SKU LIKELION. All rights reserved.</p>
+      <img src={logo} alt="logo" onClick={HomePage} />
+      <li>
+        <a
+          href="https://www.instagram.com/likelion_skuniv"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={instagram} alt="instagram" />{' '}
+        </a>
+        <a
+          href="https://github.com/LikeLion-SKU"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={github} alt="github" />{' '}
+        </a>
+      </li>
+      <p>© 2024 SKU LIKELION. All rights reserved.</p>
     </div>
   );
-};
-
-export default Footer;
+}
