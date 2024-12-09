24c17078ee36be94e7af84856c8b5b67daa6f1bc
YeChan Kim <az20058@naver.com>
2024-12-09 19:57:13 +0900

    🎨 Design : 지원서 제출 페이지 전체 width 축소

--- src/components/applicationForm/HeaderSection.jsx
+++ src/components/applicationForm/HeaderSection.jsx
@@ -3,7 +3,7 @@ import styles from '@components/applicationForm/HeaderSection.module.css';
 export default function HeaderSection() {
   return (
     <div className={styles.headerContainer}>
-      <span className={styles.header}>멋사 13기 아기사자 지원서</span>
+      <span className={styles.header}>멋사 13기 지원서</span>
       <div className={styles.buttonWrapper}>
         <button className={styles.saveBtn}>임시저장</button>
       </div>
