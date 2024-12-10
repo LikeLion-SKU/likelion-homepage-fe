fc77950e2a19e42f499f3c7664ff9300011a0e50
YeChan Kim <az20058@naver.com>
2024-12-10 18:08:51 +0900

    🎨 Design : admin/apply로 관리자 페이지 따로 분리

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,25 +1,26 @@
 import { Routes as ReactRouters, Route } from 'react-router-dom';
 
-import HomePage from "@pages/HomePage";
-import AboutPage from "@pages/AboutPage";
-import ProjectPage from "@pages/ProjectPage";
-import RecruitPage from "@pages/RecruitPage";
-import LoginPage from "@pages/LoginPage";
-import SignupPage from "@pages/SignupPage";
-import WelcomePage from "@pages/WelcomePage";
+import HomePage from '@pages/HomePage';
+import AboutPage from '@pages/AboutPage';
+import ProjectPage from '@pages/ProjectPage';
+import RecruitPage from '@pages/RecruitPage';
+import LoginPage from '@pages/LoginPage';
+import SignupPage from '@pages/SignupPage';
+import WelcomePage from '@pages/WelcomePage';
 import ApplicationForm from '../pages/applicationForm/ApplicationForm';
 import ProjectPageLayout from '../components/ProjectPage/ProjectPageLayout';
 import ProjectDetail from '../components/ProjectPage/ProjectDetail';
 import NewProjectForm from '../components/ProjectPage/NewProjectFrom';
+import ShowApply from '@pages/showApply/ShowApply';
 import { MainLayout } from '@layouts';
 
 export default function Routes() {
   return (
     <ReactRouters>
       {/* 레이아웃 공통 적용 */}
-      <Route path="/" element={<MainLayout/>}>
-        <Route index={true} element={<HomePage />}/>
-        <Route path="about" element={<AboutPage />}/>
+      <Route path="/" element={<MainLayout />}>
+        <Route index={true} element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
         <Route path="project" element={<ProjectPage />}>
           {/* 사용자 모드 */}
           <Route index element={<ProjectPageLayout isAdmin={false} />} />
@@ -29,11 +30,12 @@ export default function Routes() {
           {/* 프로젝트 상세 페이지 */}
           <Route path=":projectId" element={<ProjectDetail />} />
         </Route>
-        <Route path="recruit" element={<RecruitPage />}/>
-        <Route path="login" element={<LoginPage />}/>
-        <Route path="signup" element={<SignupPage />}/>
-        <Route path="welcome" element={<WelcomePage />}/>
+        <Route path="recruit" element={<RecruitPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="welcome" element={<WelcomePage />} />
         <Route path="apply" element={<ApplicationForm />} />
+        <Route path="admin/apply" element={<ShowApply />} />
       </Route>
     </ReactRouters>
   );
