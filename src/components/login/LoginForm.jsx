d02073925b22e85dbf163a8579d352919366fc7e
changhoyu <tyu9817@naver.com>
2024-12-27 01:34:11 +0900

    ♻️ Refactor: classNames 추가/ button disabled 기능 추가/ LoginForm.module.css작성

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -4,11 +4,17 @@ import { yupResolver } from '@hookform/resolvers/yup';
 
 import { loginSchema } from '@/constants/validationSchema';
 
+import classNames from 'classnames/bind';
+import styles from './LoginForm.module.css';
+
+const cn = classNames.bind(styles);
+
 export default function LoginForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    watch,
+    formState: { errors, isSubmitting },
   } = useForm({
     resolver: yupResolver(loginSchema),
   });
@@ -20,29 +26,71 @@ export default function LoginForm() {
    * @param {string} userData.password - 유저 비밀번호
    * @returns {Promise}
    */
-  async function sendLoginData(userData) {
+  async function onSubmit(userData) {
     // console.log() 삭제 후 로직 작성
     console.log(userData);
   }
 
   return (
-    <form onSubmit={handleSubmit(sendLoginData)}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className={styles['login-form']}
+    >
       <fieldset>
-        <legend>로그인</legend>
-        <input
-          type='text'
-          {...register('loginId')}
-        />
-        {errors.loginId?.message ? <p>{errors.loginId?.message}</p> : null}
+        <legend className={styles['login-form__title']}>로그인</legend>
 
-        <input
-          type='password'
-          {...register('password')}
-        />
-        {errors.password?.message ? <p>{errors.password?.message}</p> : null}
+        <div className={styles['login-form__inputbox']}>
+          <label
+            className={styles['login-form__label']}
+            htmlFor='loginId'
+          >
+            아이디
+          </label>
+          <input
+            className={cn(
+              'login-form__input',
+              errors.loginId?.message ? 'login-form__input--invalid' : null,
+              watch('loginId') && 'login-form__input--valid',
+            )}
+            type='text'
+            name='loginId'
+            {...register('loginId')}
+          />
+          {errors.loginId?.message ? (
+            <p className={styles['login-form__result-message--error']}>{errors.loginId.message}</p>
+          ) : null}
+        </div>
+
+        <div className={styles['login-form__inputbox']}>
+          <label
+            className={styles['login-form__label']}
+            htmlFor='password'
+          >
+            비밀번호
+          </label>
+          <input
+            className={cn(
+              'login-form__input',
+              errors.loginId?.message && 'login-form__input--invalid',
+              watch('password') && 'login-form__input--valid',
+            )}
+            type='password'
+            name='password'
+            {...register('password')}
+          />
+          {errors.password?.message ? (
+            <p className={styles['login-form__result-message--error']}>{errors.password.message}</p>
+          ) : null}
+        </div>
       </fieldset>
 
-      <button>로그인</button>
+      <button
+        className={cn('button', isSubmitting && 'button--submitting')}
+        type='submit'
+        disabled={isSubmitting}
+      >
+        로그인
+      </button>
     </form>
   );
 }
