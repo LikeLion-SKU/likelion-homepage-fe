1cea94edeee8c5e7a46638a689618a4657d37e7b
YeChan Kim <az20058@naver.com>
2025-01-11 21:53:43 +0900

    🔧 Settings : 답변 전역상태 제거

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,17 +1,6 @@
 import { create } from 'zustand';
 
 export const useStore = create((set) => ({
-  answer: [[], [], []],
-  setAnswer: (newAnswer, index, step) =>
-    set((state) => ({
-      answer: state.answer.map((stepAnswers, stepIndex) =>
-        stepIndex === step
-          ? stepAnswers.map((ans, answerIndex) =>
-              answerIndex === index ? newAnswer : ans
-            )
-          : stepAnswers
-      ),
-    })),
   step: 1,
   setStep: (nextStep) =>
     set(() => ({
