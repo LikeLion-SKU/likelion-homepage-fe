70ce74dc3fe15ffa182fdabf84edf1be546180ce
changhoyu <tyu9817@naver.com>
2025-01-19 02:11:13 +0900

    ✨ Feat: withBottomUpAnimation추가

--- src/components/home/recruit/RecruitSection.jsx
+++ src/components/home/recruit/RecruitSection.jsx
@@ -1,25 +1,15 @@
-import AnimationLayout from '@/layouts/AnimationLayout';
 import Recruit from './Recruit';
 
 export default function RecruitSection() {
   return (
-    <AnimationLayout
-      styling={{
-        position: 'absolute',
-        left: 0,
-        width: '100vw',
-        height: '100vh',
-      }}
-    >
-      <Recruit>
-        <Recruit.ItemBox>
-          <Recruit.Title />
-          <Recruit.TimerTitle />
-          <Recruit.Timer />
-          <Recruit.Button />
-        </Recruit.ItemBox>
-        <Recruit.Motion />
-      </Recruit>
-    </AnimationLayout>
+    <Recruit>
+      <Recruit.ItemBox>
+        <Recruit.Title />
+        <Recruit.TimerTitle />
+        <Recruit.Timer />
+        <Recruit.Button />
+      </Recruit.ItemBox>
+      <Recruit.Motion />
+    </Recruit>
   );
 }
