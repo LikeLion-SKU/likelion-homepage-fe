a8418c3e22c09c310641930827ac10b7ccf14ade
YeChan Kim <az20058@naver.com>
2025-01-22 00:50:28 +0900

    🎨 Design : 사용자 정보에 따른 헤더 텍스트 변경 기능 추가 및 버그 수정

--- src/hooks/useAdminApplyHook.js
+++ src/hooks/useAdminApplyHook.js
@@ -4,22 +4,12 @@ import { useEffect } from 'react';
 export function useGetApplicants(setApplicants, semester) {
   useEffect(() => {
     getApplicants(setApplicants, semester);
-  }, [setApplicants, semester]);
+  }, [semester, setApplicants]);
 }
 
-export function useGetQandA(setUserInfos, setCommonQA, setPartQA, studentId) {
+export function useGetQandA(setCommonQA, setPartQA, studentId) {
   useEffect(() => {
     const fetchUserInfo = async () => {
-      const userInfo = await getUserInfos(studentId);
-      const newUserInfo = {
-        이름: userInfo.userName,
-        학과: userInfo.department,
-        학번: userInfo.studentId,
-        전화번호: userInfo.phoneNumber,
-        이메일: userInfo.loginId + '@skuniv.ac.kr',
-      };
-      setUserInfos(newUserInfo);
-
       const res = await getAnswers(studentId, setCommonQA, setPartQA);
 
       // 배열을 필터링하여 분류
@@ -31,5 +21,22 @@ export function useGetQandA(setUserInfos, setCommonQA, setPartQA, studentId) {
     };
 
     fetchUserInfo();
-  }, [setCommonQA, setPartQA, setUserInfos, studentId]);
+  }, [setCommonQA, setPartQA, studentId]);
+}
+
+export function useGetUserInfo(setUserInfos, studentId) {
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const userInfo = await getUserInfos(studentId);
+      const newUserInfo = {
+        이름: userInfo.userName,
+        학과: userInfo.department,
+        학번: userInfo.studentId,
+        전화번호: userInfo.phoneNumber,
+        이메일: userInfo.loginId + '@skuniv.ac.kr',
+      };
+      setUserInfos(newUserInfo);
+    };
+    fetchUserInfo();
+  }, [setUserInfos, studentId]);
 }
