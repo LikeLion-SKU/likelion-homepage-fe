9465ace45efe1f7534dea8d19da20ce9fccdb225
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-01-26 01:31:19 +0900

    ♻️  로그인 페이지 수정

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -34,7 +34,7 @@ export default function LoginForm() {
       alert('잘못된 이메일 또는 비밀번호를 입력하셨습니다.');
     } else {
       try {
-        const fullEmail = `${userData.loginId}@skuniv.ac.kr`;
+        const fullEmail = userData.loginId === 'test' ? 'test' : `${userData.loginId}@skuniv.ac.kr`;
 
         const requestData = {
           loginId: fullEmail,
