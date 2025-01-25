e6674b34356b13be7cf88ab9cc0d9ed169a52456
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-01-25 19:52:55 +0900

    :bug: welcome페이지 userName 나오게 수정

--- src/components/signup/SignupSection2.jsx
+++ src/components/signup/SignupSection2.jsx
@@ -92,7 +92,7 @@ export default function SignupSection({ email, setSignupSuccess, setNow }) {
       if (response.success) {
         setSignupSuccess(true);
         setNow(1);
-        navigate('/welcome?name=${form.name}');
+        navigate(`/welcome?name=${encodeURIComponent(form.name)}`);
       } else {
         alert('회원가입에 실패하였습니다.');
       }
