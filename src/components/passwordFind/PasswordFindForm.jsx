27a3775bcf3d561619c107a672dbe664b58ab568
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-01-25 23:11:58 +0900

    ♻️  테스트용 버튼삭제

--- src/components/passwordFind/PasswordFindForm.jsx
+++ src/components/passwordFind/PasswordFindForm.jsx
@@ -356,17 +356,6 @@ export default function PasswordFindForm({ emailSuccess, setEmailSuccess, setEma
             <button className={styles['passwordFind-form__button--submittingYet']}>비밀번호 찾기</button>
           )}
         </div>
-        <button
-          style={{ cursor: 'pointer' }}
-          onClick={(e) => {
-            e.preventDefault();
-            console.log(form);
-            console.log('이동해줘!!!!');
-            setNow(2);
-          }}
-        >
-          결과 페이지로 이동~~~~
-        </button>
       </div>
     </div>
   );
