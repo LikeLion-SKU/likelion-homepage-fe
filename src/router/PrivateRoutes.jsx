d02949b3a5db19a5b07446cda52af4bcc8655824
Taegyeong Park <tgyeongt@gmail.com>
2025-01-26 17:11:29 +0900

    .

--- src/router/PrivateRoutes.jsx
+++ src/router/PrivateRoutes.jsx
@@ -3,25 +3,29 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { APIService } from '@/api/axios';
 
 export default function PrivateRoute({ children }) {
-  const [userRole, setuserRole] = useState('');
+  const [userRole, setUserRole] = useState('');
 
-    useEffect(() => {
-      async function fetchUserRole() {
-        try {
-          const baseUrl = import.meta.env.VITE_APP_GET_ROLE;
-          const response = await APIService.private.get(baseUrl);
-          setuserRole(response.data);
-        } catch (error) {
-          console.error('접근 권한이 없습니다', error);
-          location.href = '/error';
+  useEffect(() => {
+    async function fetchUserRole() {
+      try {
+        const baseUrl = import.meta.env.VITE_APP_GET_ROLE;
+        const response = await APIService.private.get(baseUrl, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`, // 토큰 추가
+          },
+        });
+        if (response.role === 'ADMIN') {
+          setUserRole('ADMIN');
         }
+      } catch (error) {
+        console.error('접근 권한이 없습니다', error);
       }
-  
-      fetchUserRole();
-    }, []);
+    }
 
-  if (userRole !== 'admin') {
-    // 인증 실패 시
+    fetchUserRole();
+  }, []);
+
+  if (userRole !== 'ADMIN') {
     return <Navigate to="/" replace />;
   }
 
