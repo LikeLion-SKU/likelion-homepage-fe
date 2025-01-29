6f3276d816a9ddc2072da3c5ed67b927c711fe9b
changhoyu <tyu9817@naver.com>
2025-01-30 01:01:21 +0900

    ✨ Feat: QuiestionsProvider.jsx / QuestionAdminPage.jsx  QuestionProvider ㅇ

--- src/pages/QuestionAdminPage.jsx
+++ src/pages/QuestionAdminPage.jsx
@@ -1,10 +1,11 @@
 import { CreatingQuestionsSection, InformationInputSection } from '@/components/creatingQuestions';
+import QuestionsProvider from '@/components/creatingQuestions/provider/QuestionsProvider';
 
 export default function QuestionAdminPage() {
   return (
-    <>
+    <QuestionsProvider>
       <InformationInputSection />
       <CreatingQuestionsSection />
-    </>
+    </QuestionsProvider>
   );
 }
