7908030f94c1e6454352717a9be3f7b28853e8ad
changhoyu <tyu9817@naver.com>
2025-01-31 02:56:23 +0900

    ✨ Feat: create SubmitApplicationBtn.jsx/ add SubmitApplicationBtn in YearInputContainer, YearInputContainer.jsx

--- src/components/creatingQuestions/informationInputSection/YearInputContainer.jsx
+++ src/components/creatingQuestions/informationInputSection/YearInputContainer.jsx
@@ -1,10 +1,12 @@
 import YearInputContainerLayout from '@/components/creatingQuestions/informationInputSection/layout/YearInputContainerLayout';
+import SubmitApplicationBtn from '@/components/creatingQuestions/informationInputSection/ui/SubmitApplicationBtn';
 import YearInput from '@/components/creatingQuestions/informationInputSection/ui/YearInput';
 
 export default function YearInputContainer() {
   return (
     <YearInputContainerLayout>
       <YearInput />
+      <SubmitApplicationBtn />
     </YearInputContainerLayout>
   );
 }
---
+++ src/components/creatingQuestions/informationInputSection/ui/SubmitApplicationBtn.jsx
@@ -0,0 +1,5 @@
+import styles from './SubmitApplicationBtn.module.css';
+
+export default function SubmitApplicationBtn() {
+  return <button className={styles['submit-btn']}>등록하기</button>;
+}
