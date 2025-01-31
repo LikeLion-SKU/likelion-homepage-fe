d4b6312e323d3e531fe2733bf336a9eb7cfa9e90
changhoyu <tyu9817@naver.com>
2025-02-01 01:31:42 +0900

    ✨ Feat: add validateAndSumbitApplication function applicationInformation value questions value in YearInputContainer SubmitApplictaionBtn, YearInputConatiner.jsx SubminApplicationBtn.jsx

--- src/components/creatingQuestions/informationInputSection/YearInputContainer.jsx
+++ src/components/creatingQuestions/informationInputSection/YearInputContainer.jsx
@@ -1,12 +1,20 @@
 import YearInputContainerLayout from '@/components/creatingQuestions/informationInputSection/layout/YearInputContainerLayout';
 import SubmitApplicationBtn from '@/components/creatingQuestions/informationInputSection/ui/SubmitApplicationBtn';
 import YearInput from '@/components/creatingQuestions/informationInputSection/ui/YearInput';
+import { useQuestions } from '@/components/creatingQuestions/provider/QuestionsProvider';
+import useApplication from '@/hooks/useApplication';
 
 export default function YearInputContainer() {
+  const { applicationInformation, questions } = useQuestions();
+  const { validateAndSubmitApplication } = useApplication();
   return (
     <YearInputContainerLayout>
       <YearInput />
-      <SubmitApplicationBtn />
+      <SubmitApplicationBtn
+        validateAndSubmitApplication={validateAndSubmitApplication}
+        applicationInformation={applicationInformation}
+        questions={questions}
+      />
     </YearInputContainerLayout>
   );
 }
--- src/components/creatingQuestions/informationInputSection/ui/SubmitApplicationBtn.jsx
+++ src/components/creatingQuestions/informationInputSection/ui/SubmitApplicationBtn.jsx
@@ -1,5 +1,14 @@
 import styles from './SubmitApplicationBtn.module.css';
 
-export default function SubmitApplicationBtn() {
-  return <button className={styles['submit-btn']}>등록하기</button>;
+export default function SubmitApplicationBtn({ validateAndSubmitApplication, applicationInformation, questions }) {
+  return (
+    <button
+      className={styles['submit-btn']}
+      onClick={function () {
+        validateAndSubmitApplication(applicationInformation, questions);
+      }}
+    >
+      등록하기
+    </button>
+  );
 }
