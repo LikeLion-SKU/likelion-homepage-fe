4833b26548ce571d7665b1cd18acdba48330b3d1
YeChan Kim <az20058@naver.com>
2025-02-01 03:01:49 +0900

    ♻️ Refactor : useEffect 구문 커스텀 훅으로 변경, 함수 선언식으로 변경 및 onClick 익명함수로 변경

--- src/components/adminApply/Generation.jsx
+++ src/components/adminApply/Generation.jsx
@@ -1,32 +1,29 @@
 import { useStore } from '@store/useStore';
 import styles from './Generation.module.css';
-import { getApplicants, getSemesters } from '@/api/adminApplyAPI';
-import { useEffect, useState } from 'react';
+import { getApplicants } from '@/api/adminApplyAPI';
+import { useState } from 'react';
+import { useGetSemesters } from '@/hooks/useAdminApplyHook';
 
 export default function Generation() {
   const [years, setYears] = useState([]);
   const { setApplicants, setSemester, setSort } = useStore();
 
-  useEffect(() => {
-    const fetchSemester = async () => {
-      const data = await getSemesters();
-      setYears(data);
-    };
-
-    fetchSemester();
-  }, []);
+  useGetSemesters(setYears);
 
   function changeSemester(semester) {
     setSort('');
     setSemester(semester);
     getApplicants(setApplicants, semester);
   }
+
   return (
     <div className={styles.navbar}>
       {years.length > 0
         ? years.map((year, index) => (
             <button
-              onClick={() => changeSemester(year)}
+              onClick={function () {
+                changeSemester(year);
+              }}
               key={index}
             >
               LIKELION SKU {year}TH
