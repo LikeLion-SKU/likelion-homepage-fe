b25d7a76d575541728d95700df0ad94242638a0b
changhoyu <tyu9817@naver.com>
2025-02-04 20:01:53 +0900

    ✨ Feat: add useNavigatehook in ApplicationBtnCard.MenuItem, ApplicationBtn

--- src/components/createApply/applicationSection/ApplicationBtn.jsx
+++ src/components/createApply/applicationSection/ApplicationBtn.jsx
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 import { useDeleteApplication, useUpdateApplicationActivation } from '@/hooks/useApplication';
 import ApplicationBtnCard from './ui/ApplicationBtnCard';
 
 export default function ApplicationBtn({ cohort, formId }) {
   const { isLoading, deleteApplication } = useDeleteApplication(formId);
   const { updateApplicationActivation } = useUpdateApplicationActivation(formId);
+
+  const nav = useNavigate();
   return (
     <ApplicationBtnCard onClick={updateApplicationActivation}>
       <ApplicationBtnCard.ApplicationCohort cohort={cohort} />
@@ -17,7 +21,13 @@ export default function ApplicationBtn({ cohort, formId }) {
           <p>로딩중</p>
         ) : (
           <>
-            <ApplicationBtnCard.MenuItem text='수정 하기' />
+            <ApplicationBtnCard.MenuItem
+              text='수정 하기'
+              onClick={function (e) {
+                e.stopPropagation();
+                nav('/admin/edit/application');
+              }}
+            />
             <ApplicationBtnCard.MenuItem
               text='삭제 하기'
               onClick={function (e) {
