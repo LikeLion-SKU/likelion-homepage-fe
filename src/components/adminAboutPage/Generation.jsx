a507853ff88318b70591c55478111fc3478721ef
sungyeon <tjddus0731@naver.com>
2025-02-07 04:00:29 +0900

    🎨 Design: 버튼 기수 수정 및 텍스트 색상 추가

--- src/components/adminAboutPage/Generation.jsx
+++ src/components/adminAboutPage/Generation.jsx
@@ -4,7 +4,7 @@ import { getProfile } from '@api/aboutAdminAPI';
 import Registration from './Registration';
 
 export default function Generation() {
-  const years = [12, 13, 14];
+  const years = [11, 12, 13];
   const [apiData, setApiData] = useState([]);
   const [selectedYear, setSelectedYear] = useState(null);
 
