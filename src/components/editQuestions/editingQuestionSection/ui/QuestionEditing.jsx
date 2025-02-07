9fcfaa36eb97057974306d627579800c8deff3ca
changhoyu <tyu9817@naver.com>
2025-02-08 00:46:45 +0900

    ✨Feat: add isEditing State, QuestionEditing.jsx

--- src/components/editQuestions/editingQuestionSection/ui/QuestionEditing.jsx
+++ src/components/editQuestions/editingQuestionSection/ui/QuestionEditing.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import styles from './QuestionEditing.module.css';
 import ButtonsContainer from '@/components/editQuestions/editingQuestionSection/ButtonsContainer';
+import CompleteBtn from '@/components/editQuestions/editingInformationInputSection/ui/CompleteBtn';
 
 export default function QuestionEditing({ content }) {
+  const [isEditing, setIsEditing] = useState(false);
   return (
     <div className={styles['question-editing-container']}>
       <input
         className={styles['question-input']}
         value={content}
       />
-      <ButtonsContainer />
+      {isEditing ? (
+        <CompleteBtn />
+      ) : (
+        <ButtonsContainer
+          setIsEditing={function () {
+            setIsEditing((prev) => !prev);
+          }}
+        />
+      )}
     </div>
   );
 }
