ac2e84cfede5a5f6466220ff5c469425d6d503c8
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-02-09 01:34:08 +0900

    :recycle: Refactor: useSignupDropdownHook.js 얼리 리턴 패턴으로 리팩토링

--- src/hooks/useSignupDropdownHook.js
+++ src/hooks/useSignupDropdownHook.js
@@ -16,18 +16,28 @@ export function handlePart(event, form, setForm, setSelectPart) {
   if (form.part === event.target.id) {
     setForm({ ...form, part: '' });
     setSelectPart('파트 선택');
-  } else {
-    setForm({ ...form, part: event.target.id });
-    if (event.target.id === '기획/디자인') {
-      setSelectPart('기획/디자인');
-    } else if (event.target.id === '프론트엔드') {
-      setSelectPart('프론트엔드');
-    } else if (event.target.id === '백엔드') {
-      setSelectPart('백엔드');
-    } else if (event.target.id === '기획') {
-      setSelectPart('기획');
-    } else if (event.target.id === '디자인') {
-      setSelectPart('디자인');
-    }
+    return;
+  }
+
+  setForm({ ...form, part: event.target.id });
+  if (event.target.id === '기획/디자인') {
+    setSelectPart('기획/디자인');
+    return;
+  }
+  if (event.target.id === '프론트엔드') {
+    setSelectPart('프론트엔드');
+    return;
+  }
+  if (event.target.id === '백엔드') {
+    setSelectPart('백엔드');
+    return;
+  }
+  if (event.target.id === '기획') {
+    setSelectPart('기획');
+    return;
+  }
+  if (event.target.id === '디자인') {
+    setSelectPart('디자인');
+    return;
   }
 }
