aee0532489506acc209e63177d748bb4f2f2dadd
changhoyu <tyu9817@naver.com>
2025-02-08 21:07:44 +0900

    ✨Feat: add useDeleteQuestionType, QuestionEditing/ add onClick event, DeleteButton.jsx

--- src/components/editQuestions/editingInformationInputSection/ui/DeleteButton.jsx
+++ src/components/editQuestions/editingInformationInputSection/ui/DeleteButton.jsx
@@ -1,9 +1,14 @@
 import { FaRegTrashAlt } from 'react-icons/fa';
 import styles from './DeleteButton.module.css';
 
-export default function DeleteButton() {
+export default function DeleteButton({ onClick }) {
   return (
-    <button className={styles['delete-btn']}>
+    <button
+      className={styles['delete-btn']}
+      onClick={function () {
+        onClick();
+      }}
+    >
       <FaRegTrashAlt className={styles['delete-btn__trashIcon']} />
     </button>
   );
