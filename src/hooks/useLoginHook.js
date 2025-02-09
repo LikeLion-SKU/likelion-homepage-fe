e7d487a49e05e781d2322073d76b6c1e603ce0cb
soojinsong <162791828+soojinsong@users.noreply.github.com>
2025-02-09 21:52:23 +0900

    :recycle: Refactor: login페이지- 로그인 버튼: 로그인 중에는 loader나오게 수정

--- src/hooks/useLoginHook.js
+++ src/hooks/useLoginHook.js
@@ -1,10 +1,11 @@
 import { APIService } from '@api/axios';
 
-export async function login(userData, errors, navigate) {
+export async function login(userData, errors, navigate, setIsLoading) {
   if (errors.loginId?.message || errors.password?.message) {
     alert('잘못된 이메일 또는 비밀번호를 입력하셨습니다.');
   } else {
     try {
+      setIsLoading(true);
       const fullEmail = userData.loginId === 'test' ? 'test' : `${userData.loginId}@skuniv.ac.kr`;
 
       const requestData = {
@@ -13,6 +14,9 @@ export async function login(userData, errors, navigate) {
       };
 
       const response = await APIService.public.post(import.meta.env.VITE_APP_LOGIN, requestData);
+      if (response) {
+        setIsLoading(false);
+      }
 
       if (response.success === true) {
         // localStorage에 토큰 저장
