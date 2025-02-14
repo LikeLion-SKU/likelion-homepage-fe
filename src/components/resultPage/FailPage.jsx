5e39f70e34265d594bee28da36891eb8263f781b
sungyeon <tjddus0731@naver.com>
2025-02-15 03:29:20 +0900

    🎨 Design: 탈락 페이지 ui및 멘트 수정

--- src/components/resultPage/FailPage.jsx
+++ src/components/resultPage/FailPage.jsx
@@ -45,14 +45,28 @@ export default function FailPage() {
 
   return (
     <div className={styles.allContainer}>
-      <div className={styles.titleContainer}>
-        <p className={styles.mainTitle}>불합격함.</p>
-      </div>
       <div className={styles.contentContainer}>
-        <p className={styles.content}>지원해주셔서 감사한데</p>
-        <p className={styles.content}>다음기회에 만나요</p>
-        <p className={styles.content}>더 할말 있을까?</p>
-        <p className={styles.content}>일단 네줄 정도...</p>
+        <p className={styles.content}>
+          {`안녕하세요 서경대학교 멋쟁이사자처럼 운영진입니다.
+
+            먼저 저희 동아리에 지원해주셔서 감사합니다.
+
+            13기 서류 결과 불합격이라는 소식을 전해드립니다.
+
+            많은 지원자로 인해 지원자님의 뛰어난 역량에도 불구하고 
+            한정된 인원 때문에 더 많은 부원들을 모시지 못하게 되어 송구스럽게 생각합니다.
+
+            다음 모집에는 더 많은 분들과 함께 하기 위해 노력하겠습니다.
+
+            다시 한번 소중한 시간을 내어 지원해주셔서 진심으로 감사드립니다!`
+            .split('\n')
+            .map((line, index) => (
+              <span key={index}>
+                {line}
+                <br />
+              </span>
+            ))}
+        </p>
       </div>
     </div>
   );
