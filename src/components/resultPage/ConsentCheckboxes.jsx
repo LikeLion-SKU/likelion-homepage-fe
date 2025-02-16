6c0a9e4457b4c511ec83c00ea06bafad111155a6
sungyeon <tjddus0731@naver.com>
2025-02-16 17:41:01 +0900

    🎨 Design: 오타 수정

--- src/components/resultPage/ConsentCheckboxes.jsx
+++ src/components/resultPage/ConsentCheckboxes.jsx
@@ -50,12 +50,12 @@ export default function ConsentCheckbox({ consents, onConsentsChange }) {
         <div className={styles.contentSection}>
           <h3 className={styles.title}>멋쟁이사자처럼 활동 참석 동의 여부</h3>
           <div className={styles.content}>
-            <p>매주 월요일 18시30분~20시30분 세션 필참</p>
-            <p>3월 21일(목) 18시 : 전체 OT 참여 권장</p>
-            <p>3월 11일(월) 교내 OT 필참</p>
+            <p>매주 월요일 18시30분~ 세션 필참</p>
+            <p>3월 17일(월) 18시 : 전체 OT 필참</p>
+            <p>3월 26일(수) 멋쟁이사자처럼 대학 전체 OT 참여 권장</p>
             <p>5월 중 진행될 아이디어톤 필참</p>
-            <p>8월 중 진행될 중앙해커톤 필참</p>
-            <p>위 활동 불참 시 불이익이 있을 수 있습니다.</p>
+            <p>8월 중 진행될 중앙 해커톤 필참</p>
+            <p>위 활동 불참 시 수료증 발급이 제한됩니다.</p>
           </div>
           <div className={styles.consentRow}>
             <input
