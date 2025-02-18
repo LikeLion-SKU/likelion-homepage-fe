8cec996dbd10eb8b9d5ebb877abe13d8bf611cf9
sungyeon <tjddus0731@naver.com>
2025-02-18 16:56:28 +0900

    🎨 Design: 합격 문구 수정

--- src/components/resultPage/interview/component/WelcomeSection.jsx
+++ src/components/resultPage/interview/component/WelcomeSection.jsx
@@ -13,11 +13,18 @@ export default function WelcomeSection() {
           함께하고 싶으신 분들이 너무 많아 지원서를 여러 번 읽어보고 모든 운영진들이 고심 끝에 결정하였습니다.
         </p>
         <p className={styles.content}>어떤 분들을 만나 뵙게 될지 기대하고 있겠습니다.</p>
-        <p className={styles.content}>아래에서 3월 9일 자정까지 반드시 2차 대면 면접 날짜를 선택 후 등록해 주세요.</p>
-        <p className={styles.content}>면접 장소는 청운관 1004호입니다.</p>
+        <p className={styles.content}>아래에서 3월 9일까지 반드시 2차 대면 면접 날짜를 선택 후 등록해 주세요.</p>
+        <p className={styles.content}>면접 장소는 다음과 같습니다.</p>
+
+        <p className={styles.content}>백엔드 학술정보관 8층 스터디룸1</p>
+        <p className={styles.content}>디자인 학술정보관 8층 스터디룸2</p>
+        <p className={styles.content}>기획 학술정보관 8층 스터디룸3</p>
+        <p className={styles.content}>프론트엔드 학술정보관 8층 스터디룸4</p>
+
         <p className={styles.content}>선택한 면접일자는 마이페이지에서 확인하실 수 있습니다.</p>
 
         <p className={styles.guidecontent}>* 면접 시간은 선착순 입니다.</p>
+        <p className={styles.guidecontent}>* 면접 시간을 선택하지 않을 경우 자동 탈락됩니다.</p>
         <p className={styles.guidecontent}>
           * 선택한 면접 시간은 마이페이지에서 3월 9일까지 취소 후 재등록 할 수 있습니다.
         </p>
