6c0a7af9490e3078d418359dc18d7cd5e0f80dfe
GENIE <hyunjin117@naver.com>
2025-02-18 23:25:16 +0900

    🎨Design: 회원가입 문구 추가

--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -6,16 +6,19 @@ export default function Signup() {
   const navigate = useNavigate();
 
   return (
-    <>
-      <p className={styles['signup-mention']}>계정이 없으신가요?</p>
-      <button
-        className={styles['signup-button']}
-        onClick={function () {
-          navigate('/signup');
-        }}
-      >
-        회원가입
-      </button>
-    </>
+    <div>
+      <div className={styles['signup-container']}>
+        <p className={styles['signup-mention']}>계정이 없으신가요?</p>
+        <button
+          className={styles['signup-button']}
+          onClick={function () {
+            navigate('/signup');
+          }}
+        >
+          회원가입
+        </button>
+      </div>
+      <p className={styles['signup-notice']}>신규 회원은 포탈 아이디가 아닌 새로 회원가입을 하셔야 합니다.</p>
+    </div>
   );
 }
